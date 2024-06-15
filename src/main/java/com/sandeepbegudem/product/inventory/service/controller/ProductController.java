12d8d6520d461e27ee59b196c7b400ca75ec1101
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-06-14 19:20:21 -0500

    chore: update the line separator

--- src/main/java/com/sandeepbegudem/product/inventory/service/controller/ProductController.java
+++ src/main/java/com/sandeepbegudem/product/inventory/service/controller/ProductController.java
@@ -1,1 +1,53 @@
-package com.sandeepbegudem.product.inventory.service.controller;import com.sandeepbegudem.product.inventory.service.entity.Product;import com.sandeepbegudem.product.inventory.service.service.ProductService;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.graphql.data.method.annotation.Argument;import org.springframework.graphql.data.method.annotation.MutationMapping;import org.springframework.graphql.data.method.annotation.QueryMapping;import org.springframework.http.HttpStatus;import org.springframework.http.ResponseEntity;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PathVariable;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;import java.util.List;//@RestController//@RequestMapping("/api/v1/products/graphql")@Controllerpublic class ProductController {    private final  ProductService productService;    @Autowired    public ProductController(ProductService productService) {        this.productService = productService;    }    @QueryMapping    public List<Product> retrieveAllProducts() {        return productService.fetchAllProducts();    }    @QueryMapping    public List<Product> retrieveAllProductsByCategory(@Argument String category) {        return productService.fetchProductsByCategory(category);    }    @MutationMapping    public Product updateProductStockById(@Argument int id, @Argument int stock) {        return productService.updateProductStockByProductId(id, stock);    }//    @MutationMapping//    public Product createNewProduct(@Argument int id, @Argument int stockQuantity) {//        return productService.updateProductStockByProductId(id, stockQuantity);//    }}
+package com.sandeepbegudem.product.inventory.service.controller;
+
+import com.sandeepbegudem.product.inventory.service.entity.Product;
+import com.sandeepbegudem.product.inventory.service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//@RestController
+//@RequestMapping("/api/v1/products/graphql")
+@Controller
+public class ProductController {
+
+    private final  ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @QueryMapping
+    public List<Product> retrieveAllProducts() {
+        return productService.fetchAllProducts();
+    }
+
+    @QueryMapping
+    public List<Product> retrieveAllProductsByCategory(@Argument String category) {
+        return productService.fetchProductsByCategory(category);
+    }
+
+    @MutationMapping
+    public Product updateProductStockById(@Argument int id, @Argument int stock) {
+        return productService.updateProductStockByProductId(id, stock);
+    }
+
+//    @MutationMapping
+//    public Product createNewProduct(@Argument int id, @Argument int stockQuantity) {
+//        return productService.updateProductStockByProductId(id, stockQuantity);
+//    }
+
+
+
+}
--- src/main/java/com/sandeepbegudem/product/inventory/service/entity/Product.java
+++ src/main/java/com/sandeepbegudem/product/inventory/service/entity/Product.java
@@ -1,1 +1,31 @@
-package com.sandeepbegudem.product.inventory.service.entity;import jakarta.persistence.Entity;import jakarta.persistence.GeneratedValue;import jakarta.persistence.GenerationType;import jakarta.persistence.Id;import lombok.AllArgsConstructor;import lombok.Data;import lombok.NoArgsConstructor;@AllArgsConstructor@NoArgsConstructor@Data@Entitypublic class Product {    @Id    @GeneratedValue(strategy = GenerationType.IDENTITY)    private Integer id;    private String name;    private String category;    private float price;    private int stock;    public Product(String name, String category, float price, int stock) {        this.name = name;        this.category = category;        this.price = price;        this.stock = stock;    }}
+package com.sandeepbegudem.product.inventory.service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String category;
+    private float price;
+    private int stock;
+
+    public Product(String name, String category, float price, int stock) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+    }
+}
